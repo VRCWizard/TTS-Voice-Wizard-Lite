0baa3e670999e77dc0f3a68c1c1710449c7491a1
VRCWizard <101527472+VRCWizard@users.noreply.github.com>
2022-06-12 22:49:47 -0500

    adding solution

---
+++ OSCVRCWizFree/Program.cs
@@ -0,0 +1,27 @@
+using System.Speech.Recognition;
+
+namespace TTSWizardFree
+{
+    internal static class Program
+    {
+
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        /// 
+        [STAThread]
+
+        static void Main()
+        {
+           
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+          
+         
+            ApplicationConfiguration.Initialize();
+            Application.Run(new VoiceWizardWindow());
+        }
+
+    
+    }
+}
